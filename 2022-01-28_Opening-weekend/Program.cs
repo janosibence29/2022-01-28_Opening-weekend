0b22bb79af5845441f95ba226d70b84b4d197d50
janosibence29 <92314900+janosibence29@users.noreply.github.com>
2022-01-28 09:12:20 +0100

    Add project files.

---
+++ 2022-01-28_Opening-weekend/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
